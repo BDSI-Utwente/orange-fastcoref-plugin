6b3fa56bd08c87688aeffefd2f925c68dfc4653b
Karel Kroeze <karel.kroeze@gmail.com>
2025-06-28 09:21:00 +0200

    handle coref in thread, add progressbar, refactor UI

--- orangecontrib/fastcoref/widgets/coreference_resolution.py
+++ orangecontrib/fastcoref/widgets/coreference_resolution.py
@@ -2,20 +2,21 @@ from functools import partial
 from Orange.data import Table
 from Orange.widgets import gui
 from concurrent.futures import Future, wait
-from Orange.widgets.utils.concurrent import ThreadExecutor, FutureWatcher
-from Orange.widgets.settings import Setting
+from Orange.widgets.utils.concurrent import ThreadExecutor, FutureWatcher, methodinvoke
+from Orange.widgets.settings import Setting, ContextSetting, DomainContextHandler
 from Orange.widgets.widget import OWWidget, Input, Output, Msg
 from AnyQt.QtCore import Qt, pyqtSlot, QThread
 from AnyQt import QtCore
+import traceback
 
-from orangecontrib.text.corpus import Corpus  # type: ignore warning from shared namespace
+from orangecontrib.text.corpus import Corpus              # type: ignore warning from shared namespace
 from orangecontrib.text.preprocess import BASE_TOKENIZER  # type: ignore warning from shared namespace
 
 import spacy
 import spacy.cli
 import spacy.cli.download
 
-from fastcoref import spacy_component
+from fastcoref import spacy_component  # noqa: F401 imported for side-effects
 
 import pandas as pd
 
@@ -32,6 +33,34 @@ FASTCOREF_MODELS = ["fastcoref", "lingmess"]
 
 DEFAULT_SPACY_MODEL = "nl_core_news_md"
 DEFAULT_FASTCOREF_MODEL = "fastcoref"
+
+LABEL_STYLE_COLOURS = {
+  "inactive": {
+    "fg": "#6B7280",
+    "bg": "#F3F4F6"
+  },
+  "active": {
+    "fg": "#2563EB",
+    "bg": "#DBEAFE"
+  },
+  "success": {
+    "fg": "#059669",
+    "bg": "#D1FAE5"
+  },
+  "error": {
+    "fg": "#DC2626",
+    "bg": "#FEE2E2"
+  }
+}
+LABEL_STYLE_TEMPLATE = """ QLabel {{
+    padding: 0.2em; margin-bottom: 6px;
+    color: {fg}; border: 1px solid {fg}; background-color: {bg};
+}} """
+
+LABEL_STYLE_INACTIVE = LABEL_STYLE_TEMPLATE.format(**LABEL_STYLE_COLOURS["inactive"])
+LABEL_STYLE_ACTIVE = LABEL_STYLE_TEMPLATE.format(**LABEL_STYLE_COLOURS["active"])
+LABEL_STYLE_SUCCESS = LABEL_STYLE_TEMPLATE.format(**LABEL_STYLE_COLOURS["success"])
+LABEL_STYLE_ERROR = LABEL_STYLE_TEMPLATE.format(**LABEL_STYLE_COLOURS["error"])
 
 
 class Task:
@@ -69,11 +98,17 @@ class CoreferenceResolutionWidget(OWWidget):
     _executor: ThreadExecutor  # executor for running tasks in a separate thread
 
     # settings
-    commitOnChange = Setting(True)  # commit on change of inputs
-    coref_model = Setting(
-        DEFAULT_FASTCOREF_MODEL
-    )  # model to use for coreference resolution
-    spacy_model = Setting(DEFAULT_SPACY_MODEL)  # spaCy model to use for tokenization
+    commitOnChange = Setting(True)
+    coref_model = Setting(DEFAULT_FASTCOREF_MODEL) 
+    spacy_model = Setting(DEFAULT_SPACY_MODEL)  
+
+    # 'content' column for input data, depends on domain
+    settingsHandler = DomainContextHandler()
+    content_attr = ContextSetting("Text")
+
+    class TaskCancelledException(Exception): 
+        def __init__(self): 
+            super().__init__("task cancelled")
 
     class Inputs:
         # specify the name of the input and the type
@@ -102,17 +137,24 @@ class CoreferenceResolutionWidget(OWWidget):
         self.corpus = None
         self.nlp = None
         self.spacy_model = DEFAULT_SPACY_MODEL
+        self.spacy_model_status: str = "no spacy model loaded"
         self.coref_model = DEFAULT_FASTCOREF_MODEL
+        self.coref_model_status: str = "no coref model loaded"
 
         self.controlArea.layout().setAlignment(Qt.AlignmentFlag.AlignTop)
         self.controlArea.setLayoutDirection(QtCore.Qt.LayoutDirection.LeftToRight)
-        # self.controlArea.setLayout(QtWidgets.QHBoxLayout())
-        self.groupbox_settings = gui.vBox(self.controlArea, box="Settings")
-        self.groupbox_settings.setMinimumSize(QtCore.QSize(250, 0))
-        self.groupbox_settings.layout().setAlignment(Qt.AlignmentFlag.AlignTop)
+        
+        # group container for model-related settings
+        self.groupbox_model_settings = gui.vBox(self.controlArea, box="Model settings")
+        self.groupbox_model_settings.setMinimumSize(QtCore.QSize(250, 0))
+        self.groupbox_model_settings.layout().setAlignment(Qt.AlignmentFlag.AlignTop)
 
+        # row for spacy model selection and status
+        # spacy_row: QtWidgets.QGroupBox
+        # spacy_row = gui.hBox(self.groupbox_model_settings, spacing = 12)
+        
         gui.comboBox(
-            self.groupbox_settings,
+            self.groupbox_model_settings,
             self,
             "spacy_model",
             items=SPACY_MODELS,
@@ -120,13 +162,13 @@ class CoreferenceResolutionWidget(OWWidget):
             callback=self.spacy_model_changed,
             sendSelectedValue=True,
         )
+        self.spacy_model_status_label = gui.label(self.groupbox_model_settings, self, "%(spacy_model_status)s")
+        self.spacy_model_status_label.setStyleSheet(LABEL_STYLE_INACTIVE)
 
-        self.spacy_model_status_label = gui.label(self.groupbox_settings, self, "")
-        # self.spacy_model_status_label.setStyleSheet()
-        # self.spacy_model_status_label.setVisible(False)
-
+        # row for coref model and status
+        # coref_row = gui.hBox(self.groupbox_model_settings, spacing = 12)
         gui.comboBox(
-            self.groupbox_settings,
+            self.groupbox_model_settings,
             self,
             "coref_model",
             items=FASTCOREF_MODELS,
@@ -135,40 +177,84 @@ class CoreferenceResolutionWidget(OWWidget):
             sendSelectedValue=True,
         )
 
-        # self.coref_model_status_label = gui.label(self.groupbox_settings, self, "")
+        self.coref_model_status_label = gui.label(self.groupbox_model_settings, self, "%(coref_model_status)s")
+        self.coref_model_status_label.setStyleSheet(LABEL_STYLE_INACTIVE)
+
+        # self.coref_model_status_label = gui.label(self.groupbox_model_settings, self, "")
         # self.coref_model_status_label.setVisible(False)
+        
+        # group container for domain-related settings
+        self.groupbox_domain_settings = gui.vBox(self.controlArea, box="Domain settings")
+        self.groupbox_domain_settings.setMinimumSize(QtCore.QSize(250, 0))
+        self.groupbox_domain_settings.layout().setAlignment(Qt.AlignmentFlag.AlignTop)
+
+        gui.comboBox(self.groupbox_domain_settings, self, "content_attr", label="Content attribute name", sendSelectedValue=True)
 
         gui.checkBox(
-            self.groupbox_settings,
+            self.groupbox_domain_settings,
             self,
             "commitOnChange",
-            "Commit on change",
+            "Auto run on changes",
             callback=self.setting_changed,
         )
 
-        gui.button(self.groupbox_settings, self, "Run", self._start_coref_resolution)
+        self.start_button = gui.button(self.groupbox_domain_settings, self, "Start", self._start_coref_resolution)
+        self.start_button.setDisabled(self.commitOnChange)
+
+        self.stop_button = gui.button(self.groupbox_domain_settings, self, "Stop", self._stop_coref_resolution)
+        self.stop_button.setVisible(False)
+        
 
     @Inputs.corpus
-    def set_corpus(self, corpus):
+    def set_corpus(self, corpus: Corpus):
+        self.closeContext()
+        
         if corpus:
             self.corpus = corpus
+
+            # clear current items
+            self.controls.content_attr.clear()
+            self.controls.content_attr.addItems([meta.name for meta in self.corpus.domain.metas])
+
+            # apply context (now that correct options are available, that should hopefully update the selected element)
+            self.openContext(corpus.domain)
+                                 
         else:
             self.corpus = None
 
     def setting_changed(self):
+        self.start_button.setDisabled(self.commitOnChange)
+        self.update_if_needed()
+
+    def update_if_needed(self): 
         if self.commitOnChange:
-            self.commit()
+            self._start_coref_resolution()
 
     def spacy_model_changed(self):
+        self.coref_model_status = "waiting for spacy model..."
+        self.coref_model_status_label.setStyleSheet(LABEL_STYLE_INACTIVE)
         self._start_spacy_load_task(self.spacy_model)
 
-    def _cancel_or_await_current_task(self):
+    def cancel_or_await_previous_task(self):
         """Cancel the current task if it is running, or wait for it to finish."""
         if self._task is not None:
             self._task.cancel()
+            
+            # the done signal may or may not be connected to anything. 
+            # NOTE: I'm also not convinced this is needed considering we already call 
+            # _task.watcher.disconnect() in _task.cancel(). For now, keep the disconnect
+            # call and ignore errors raised for lack of connections.
+            try:
+                self._task.watcher.done.disconnect()
+            except TypeError as err: 
+                print(f"failed to disconnect signals: {err}.")
+
             assert self._task.future.done()
-            self._task.watcher.done.disconnect()
             self._task = None
+        
+        # cleanup any lingering status messages/progress bars
+        self.clear_messages()
+        self.progressBarFinished()
 
     def _assert_task_finished(self, future):
         """Assert that the current task has finished."""
@@ -179,13 +265,18 @@ class CoreferenceResolutionWidget(OWWidget):
 
     def _start_spacy_load_task(self, model_name):
         """Start a task to load the spaCy model."""
-        self._cancel_or_await_current_task()
+        self.cancel_or_await_previous_task()
 
         if not spacy.util.is_package(self.spacy_model):
             self._start_spacy_download_task(self.spacy_model)
             return
 
         self.Information.spacy_load_model(model_name)
+        self.spacy_model_status = f"loading model: {model_name}..."
+        self.spacy_model_status_label.setStyleSheet(LABEL_STYLE_ACTIVE)
+        self.setInvalidated(True)
+        self.setReady(False)
+
         self._task = task = Task()
         task.future = self._executor.submit(spacy.load, model_name)
         task.watcher = FutureWatcher(task.future)
@@ -199,6 +290,8 @@ class CoreferenceResolutionWidget(OWWidget):
         # clear task, info message
         self._task = None
         self.Information.spacy_load_model.clear()
+        self.spacy_model_status = f"{self.spacy_model} ready"
+        self.spacy_model_status_label.setStyleSheet(LABEL_STYLE_SUCCESS)
 
         # load the spaCy model
         self.nlp = future.result()
@@ -207,9 +300,14 @@ class CoreferenceResolutionWidget(OWWidget):
         self._start_coref_load_task(self.coref_model)
 
     def _start_spacy_download_task(self, model_name):
-        self._cancel_or_await_current_task()
+        self.cancel_or_await_previous_task()
 
         self.Information.spacy_download_model(model_name)
+        self.spacy_model_status = f"downloading model: {model_name}..."
+        self.spacy_model_status_label.setStyleSheet(LABEL_STYLE_ACTIVE)
+        self.setInvalidated(True)
+        self.setReady(False)
+
         self._task = task = Task()
         task.future = self._executor.submit(spacy.cli.download, model_name)
         task.watcher = FutureWatcher(task.future)
@@ -222,6 +320,8 @@ class CoreferenceResolutionWidget(OWWidget):
         # clear task, info message
         self._task = None
         self.Information.spacy_download_model.clear()
+        self.spacy_model_status = f"downloading model: {self.spacy_model} completed"
+        self.spacy_model_status_label.setStyleSheet(LABEL_STYLE_SUCCESS)
 
         # start loading the spaCy model
         self._start_spacy_load_task(self.spacy_model)
@@ -230,7 +330,7 @@ class CoreferenceResolutionWidget(OWWidget):
         self._start_coref_load_task(self.coref_model)
 
     def _start_coref_load_task(self, model_name):
-        self._cancel_or_await_current_task()
+        self.cancel_or_await_previous_task()
 
         # if we don't have a spacy model loaded, do that first
         # note that the task finished handler for the spacy model
@@ -241,6 +341,10 @@ class CoreferenceResolutionWidget(OWWidget):
 
         # start coreference model loading
         self.information(f"{model_name} loading...")
+        self.coref_model_status = f"loading model: {model_name}"
+        self.coref_model_status_label.setStyleSheet(LABEL_STYLE_ACTIVE)
+        self.setInvalidated(True)
+        self.setReady(False)
 
         # define a worker function to load the coreference model
         def _coref_load_worker(nlp: spacy.language.Language, model_name: str):
@@ -277,6 +381,14 @@ class CoreferenceResolutionWidget(OWWidget):
         # clear task, info message
         self._task = None
         self.Information.clear()
+        self.coref_model_status = f"{self.coref_model} ready"
+        self.coref_model_status_label.setStyleSheet(LABEL_STYLE_SUCCESS)
+
+        # notify orange that we're ready to receive data
+        self.setReady(True)
+
+        # run coref resolution?
+        self.update_if_needed()
 
     def _start_coref_resolution(self):
         if self.corpus is None:
@@ -285,7 +397,8 @@ class CoreferenceResolutionWidget(OWWidget):
         # if nlp or coref model isn't loaded yet, do that first
         # TODO: we currently start just the nlp/coref load task, then return
         #   should we implement some way of queueing the resolution task?
-        self._cancel_or_await_current_task()
+        self.cancel_or_await_previous_task()
+
         if self.nlp is None:
             self._start_spacy_load_task(self.spacy_model)
             return
@@ -294,19 +407,19 @@ class CoreferenceResolutionWidget(OWWidget):
             self._start_coref_load_task(self.coref_model)
             return
 
-        def _coref_resolution_worker(corpus: Corpus, nlp: spacy.language.Language):
+        def _coref_resolution_worker(corpus: Corpus, nlp: spacy.language.Language, content_attr, callback):
             """Process the corpus with the spaCy pipeline and resolve coreferences."""
 
             resolved_corpus = corpus.copy()
             coreferences = []
+            total = len(corpus)
 
-            for doc_idx, doc in enumerate(corpus):
-                # Process each document in the corpus with the spaCy pipeline
-                resolved_doc = nlp(
-                    doc["Text"].value,
-                    component_cfg={"fastcoref": {"resolve_text": True}},
-                )
-                resolved_corpus[doc_idx]["Text"] = resolved_doc._.resolved_text
+            # pluck all the texts out of the corpus
+            docs = [doc[content_attr].value for doc in corpus]
+            config = { "batch_size": 1, "component_cfg": {"fastcoref": {"resolve_text": True}}}
+
+            for doc_idx, doc, resolved_doc in zip(range(len(corpus)), docs, nlp.pipe(docs, **config)):
+                resolved_corpus[doc_idx][content_attr] = resolved_doc._.resolved_text
 
                 # Extract coreference clusters
                 for cluster_idx, cluster in enumerate(resolved_doc._.coref_clusters):
@@ -320,6 +433,10 @@ class CoreferenceResolutionWidget(OWWidget):
                                 "text": resolved_doc.text[start:end],
                             }
                         )
+
+                if callback:
+                    # update progress
+                    callback((doc_idx + 1)/total * 100)
 
             # re-tokenize the resolved corpus
             resolved_corpus.name = f"{corpus.name} (resolved coreferences)"
@@ -335,31 +452,82 @@ class CoreferenceResolutionWidget(OWWidget):
 
             return resolved_corpus, coreferences
 
-        # Start the coreference resolution task
-        # TODO: hook up progress bar
-        self.information("Resolving coreferences...")
         self._task = task = Task()
+
+        # create an invoke wrapper to safely call QtObject methods across threads
+        set_progress = methodinvoke(self, "progressBarSet", (float,))
+
+        # callback handler to check for task cancellation and update progress
+        def callback(progress):
+            if task.cancelled:
+                raise self.TaskCancelledException()
+            set_progress(progress)
+            
+        # Start the coreference resolution task
+        self.information("Resolving coreferences...")
+        self.progressBarInit() # assumes 100 'total'
+        self.start_button.setVisible(False)
+        self.stop_button.setVisible(True)
+        self.stop_button.setEnabled(True)
+        self.setReady(False)
+        self.setInvalidated(True)
+        
         task.future = self._executor.submit(
-            partial(_coref_resolution_worker, self.corpus, self.nlp)
+            partial(_coref_resolution_worker, self.corpus, self.nlp, self.content_attr, callback)
         )
         task.watcher = FutureWatcher(task.future)
         task.watcher.done.connect(self._coref_resolution_finished)
+
+    def _stop_coref_resolution(self):
+        if not self._task: 
+            return
+
+        self.stop_button.setEnabled(False)
+        self._task.cancelled = True
+        self.setReady(True)
+
 
     @pyqtSlot(Future)
     def _coref_resolution_finished(self, future: Future):
         """Handle the completion of the coreference resolution task."""
         self._assert_task_finished(future)
         self._task = None  # clear the task
+        self.progressBarFinished()
+        self.start_button.setVisible(True)
+        self.start_button.setDisabled(self.commitOnChange)
+        self.stop_button.setVisible(False)
+        self.setReady(True)
+        self.setInvalidated(False)
 
-        resolved_corpus, coreferences = future.result()
+        # try getting the result
+        try: 
+            resolved_corpus, coreferences = future.result()
+            self.Outputs.resolved_corpus.send(resolved_corpus)
+            self.Outputs.coreferences.send(coreferences)
+        except Exception as err:
+            if not isinstance(err, self.TaskCancelledException):
+                self.error(f"Error during coreference resolution: {err}")
+                traceback.print_exc()
+        finally: 
+            self.Information.clear()
 
-        self.Information.clear()
-        self.Outputs.resolved_corpus.send(resolved_corpus)
-        self.Outputs.coreferences.send(coreferences)
+    def handleNewSignals(self): 
+        if self.corpus is None: 
+            self.Outputs.resolved_corpus.send(None)
+            self.Outputs.coreferences.send(None)
+            return
 
-    def commit(self):
-        self.Outputs.resolved_corpus.send(self.corpus)
-        self.Outputs.coreferences.send(self.corpus)
+        if self.commitOnChange:
+            self._start_coref_resolution()
+        else:
+            self.setInvalidated(True)
+
+    def setInvalidated(self, state: bool):
+        # whenever we invalidate, we also want to clear our outputs
+        if state:
+            self.Outputs.resolved_corpus.send(None)
+            self.Outputs.coreferences.send(None)
+        super().setInvalidated(state)
 
     def send_report(self):
         # self.report_plot() includes visualizations in the report
